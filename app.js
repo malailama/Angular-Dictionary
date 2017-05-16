c552489211fe945dfb848c7fa7def1b989f850a9
malAIlama <akshay1301@gmail.com>
2017-05-16 19:28:24 +0530

    Merriam Dictionary Web API integrated. Data format is XML

--- app.js
+++ app.js
@@ -4,6 +4,8 @@
 
 	console.log("Controller was loaded succesfully");
 
+	var base_url = "https://od-api.oxforddictionaries.com/api/v1";
+
 	$scope.init = function init(){
 		//console.log("init() was called");
 		$scope.search = "Entrepreneur";
@@ -20,13 +22,32 @@
 			$scope.validFlag = true;
 		}
 		
-		console.log("Value of validFlag: ",$scope.validFlag);
+		//console.log("Value of validFlag: ",$scope.validFlag);
 		return $scope.validFlag;
 	};
 
 	$scope.searchApi = function searchApi(){
-		fetch();
+
+		console.log("SearchAPI was called");
+
+		$http({
+			method: 'GET',
+			url: 'http://www.dictionaryapi.com/api/v1/references/collegiate/xml/'+$scope.search+'?key=1743f7d3-3c20-426e-9b9a-d27c25c0806d',
+		}).
+		success(function(response){
+			$scope.data = response.data;
+			console.log("Data received successfully",typeof $scope.data);
+		});
 	};
+
+	//function fetch(){
+	//	console.log("fetch was called");
+	//	$http.get("https://od-api.oxforddictionaries.com/api/v1/entries/en/city")
+	//	.then(function successCallback(response) {
+	//		console.log("Successful response");
+	//		$scope.meaning =response.data;
+	//		});
+	//	};
 
 	});
 })();
