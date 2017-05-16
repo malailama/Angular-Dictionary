8cac4ac3612589ebc13db6f8f54172c6d770241f
malAIlama <akshay1301@gmail.com>
2017-05-16 17:10:27 +0530

    Input validations applied

--- app.js
+++ app.js
@@ -1,7 +1,32 @@
 (function(){
 	angular.module('dictionaryApp',[])
-	.controller('dictionaryController', function($scope){
+	.controller('dictionaryController', function($scope, $http){
 
 	console.log("Controller was loaded succesfully");
+
+	$scope.init = function init(){
+		//console.log("init() was called");
+		$scope.search = "Entrepreneur";
+		$scope.validFlag = true;
+	}
+
+	$scope.validateInput = function validateInput(){
+		//console.log("validateInput was called");
+
+		if($scope.search === undefined || $scope.search === '' || $scope.search === null){
+			$scope.validFlag = false;
+		}
+		else{
+			$scope.validFlag = true;
+		}
+		
+		console.log("Value of validFlag: ",$scope.validFlag);
+		return $scope.validFlag;
+	};
+
+	$scope.searchApi = function searchApi(){
+		fetch();
+	};
+
 	});
 })();
