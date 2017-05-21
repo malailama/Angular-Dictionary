60b20bad361c7ea95626402f671f4bce63a5b1cf
malailama <akshay1301@gmail.com>
2017-05-21 20:00:41 +0530

    Dictionary 1.0 complete

--- app.js
+++ app.js
@@ -2,35 +2,45 @@
 	angular.module('dictionaryApp',[])
 	.controller('dictionaryController', function($scope, $http){
 
-	console.log("Controller was loaded succesfully");
+	//console.log("Controller was loaded succesfully");
 
 	var base_url = "https://od-api.oxforddictionaries.com/api/v1";
 
 	$scope.init = function init(){
 		//console.log("init() was called");
-		$scope.search = "Ambition";
-		$scope.validFlag = true;
-		$scope.definition = null;
-		$scope.showFlag = false;
+		$scope.search = null;
+		$scope.validFlag = false;
+		$scope.showFlag = true;
+		$scope.wordNotFound = false;
 	}
 
 	$scope.validateInput = function validateInput(){
 		//console.log("validateInput was called");
+		$scope.showFlag = false;
+		var searchTerms = $scope.search.split(" ");
 
 		if($scope.search === undefined || $scope.search === '' || $scope.search === null){
 			$scope.validFlag = false;
 		}
+		else if($scope.search.length && searchTerms.length > 1){
+			//Validate for a single word
+			$scope.errorMessage= "We currently are a single word dictionary. Please bear with us till our search monkeys become smart enough";
+			$scope.wordNotFound = true;
+			$scope.validFlag = false;
+		}
 		else{
 			$scope.validFlag = true;
+			$scope.wordNotFound = false;
 		}
 		
 		//console.log("Value of validFlag: ",$scope.validFlag);
+
 		return $scope.validFlag;
 	};
 
 	$scope.searchApi = function searchApi(){
-
-		console.log("SearchAPI was called");
+		
+		//console.log("SearchAPI was called");
 
 		var merriamURL = 'http://www.dictionaryapi.com/api/v1/references/collegiate/xml/'+$scope.search+'?key=1743f7d3-3c20-426e-9b9a-d27c25c0806d';
 		var pearsonURL = 'https://api.pearson.com/v2/dictionaries/ldoce5/entries?headword='+$scope.search+'&apikey=4W4QfkL0w0RDEkQyEQo2GBIEJnwGeGiZ';
@@ -39,16 +49,12 @@
 			method: 'GET',
 			url: pearsonURL,
 		})
-		//.success(function(response){
-		//	$scope.data = response.details;
-		//	$scope.details = angular.fromJson($scope.data);
-		//	console.log("Data received successfully", $scope.details);
-		//	console.log("Meaning: :", $scope.details[status]);
-		//});
+
 		.then(function successCallBack(response){
 			//console.log("Response received successfully: ",typeof response.data);
 			//console.log('Data received :', response.data.results);
-			$scope.data = response.data.results;
+			$scope.dataDump = response.data;
+			$scope.data = angular.fromJson($scope.dataDump);
 			$scope.showResult($scope.data);
 			//console.log('definition :', $scope.definition);
 
@@ -57,23 +63,65 @@
 		});
 
 		$scope.showResult = function(data){
-			console.log("showResult was called !");
-			$scope.exampleText = null;
+			//console.log("showResult was called !");
+			$scope.wordArray = [];
+			$scope.wordNotFound = false;
 
-			console.log("Value of showFlag: ", $scope.showFlag);
+			if(data.total>0){
+				//console.log("No of result: ", data.total);
+				//console.log("result: "+data.results[1].senses[0].definition[0]);
+
+				//Adding all the wordObjects to wordArray
+				for(var wordCount = 0; wordCount<data.total; wordCount++){
+					//Initialising Variables
+					$scope.wordObject = {};
+					$scope.word = null;
+					$scope.exampleText = null;
+					$scope.def = null;
+					//Fetch headword
+					$scope.word = data.results[wordCount].headword;
+					//Fetch Definition
+					$scope.def = data.results[wordCount].senses[0].definition[0];
+					//Fetch example text
+					if(data.results[wordCount].senses[0].collocation_examples){
+						$scope.exampleText = data.results[wordCount].senses[0].collocation_examples[0].example.text;
+					}
+					else if(data.results[wordCount].senses[0].gramatical_examples){
+						$scope.exampleText = data.results[wordCount].senses[0].gramatical_examples[0].examples[0].text;
+					}
+					else if(data.results[wordCount].senses[0].examples){
+						$scope.exampleText = data.results[wordCount].senses[0].examples[0].text;
+					}
+					else{
+						$scope.exampleText = "Sorry, no example available";
+					}
+					//Create wordObject
+					$scope.wordObject = {
+						'headword':$scope.word,
+						'definition':$scope.def,
+						'example':$scope.exampleText
+					};
+					//Push word object in WordArray
+					//console.log($scope.wordObject.headword, $scope.wordObject.definition,$scope.wordObject.example);
+					$scope.wordArray.push($scope.wordObject);
+					//console.log("wordArray length",$scope.wordArray.length);
+				}
+
+				$scope.showFlag = true;
+				$scope.wordNotFound = false;
+			}else{
+				//Show an error message on screen
+				console.log("No result found");
+				$scope.showFlag = false;
+				$scope.wordNotFound = true;
+				$scope.errorMessage = "Sorry, we cannot quench your thirst for knowledge. No traces of '"+$scope.search+"' were found in the known universe of dictionaries";
+			}
+
+			//console.log("Value of showFlag: ", $scope.showFlag);
 			return $scope.showFlag;
 		};
 
 	};
-
-	//function fetch(){
-	//	console.log("fetch was called");
-	//	$http.get("https://od-api.oxforddictionaries.com/api/v1/entries/en/city")
-	//	.then(function successCallback(response) {
-	//		console.log("Successful response");
-	//		$scope.meaning =response.data;
-	//		});
-	//	};
 
 	});
 })();
